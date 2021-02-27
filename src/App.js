5039bc7fa74b34ca7ad213ac5b0eb4987d32d261
Power1stZ <61010568@kmitl.ac.th>
2021-02-27 16:19:34 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ku create laew na i sus
+          Sawasdee TOC Itim
         </p>
         <a
           className="App-link"
