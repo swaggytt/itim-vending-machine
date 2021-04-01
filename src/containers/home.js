e10e1d661188baa0db2096cd4a927cb0d03f236e
swaggytt <ichromex@hotmail.com>
2021-04-01 18:38:23 +0700

    conflict resolve

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,24 +1,22 @@
 import Diagram from "../components/Diagram";
 import LogoNavbar from "../components/LogoNavbar";
 import "./home.css";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Home() {
-
-  const [key,setKey] = useState(0)
+  const [key, setKey] = useState(0);
 
   return (
-    
-      <div>
     <div className="container">
       <div className="state-diagram">
-        <Diagram value={key}/>
-        <button onClick={() => setKey(prev=>prev=1)}>Cone</button>
+        <Diagram value={key} />
+        <button onClick={() => setKey((prev) => (prev = 1))}>Cone</button>
         <button>Bread</button>
         <button>StickyRice</button>
         <button>Choco</button>
         <button>Vanilla</button>
         <button>Accept</button>
       </div>
+    </div>
   );
 }
