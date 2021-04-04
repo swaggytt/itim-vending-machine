9199de4c867489d0e48a5e319f18d0a382f405d0
Puwadol Limwanichsin <45484127+AlbertPuwadol@users.noreply.github.com>
2021-04-04 07:23:09 +0700

    edit mockup button

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,21 +1,30 @@
 import Diagram from "../components/Diagram";
 import "./home.css";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import useForceUpdate from "../function/useForceUpdate"
 
 export default function Home() {
 
-  const [key,setKey] = useState(0)
+  const [input,setInput] = useState("start");
+  const [click, setClick] = useState(true);
+  const {key, forceUpdate} = useForceUpdate();
+
+  useEffect(() => {
+    forceUpdate()
+    // console.log("force")
+  }, [click])
 
   return (
     <div key={key} className="container">
-      <div className="state-diagram">
-        <Diagram value={key}/>
-        <button onClick={() => setKey(1)}>Cone</button>
-        <button onClick={() => setKey(2)}>Bread</button>
-        <button>StickyRice</button>
-        <button>Choco</button>
-        <button>Vanilla</button>
-        <button>Accept</button>
+      <div  className="state-diagram">
+        <Diagram value={input} click={click}/>
+        <button onClick={() => {setInput("cone");setClick(!click)}}>Cone</button>
+        <button onClick={() => {setInput("bread");setClick(!click)}}>Bread</button>
+        <button onClick={() => {setInput("sticky");setClick(!click)}}>StickyRice</button>
+        <button onClick={() => {setInput("choco");setClick(!click)}}>Choco</button>
+        <button onClick={() => {setInput("vanilla");setClick(!click)}}>Vanilla</button>
+        <button onClick={() => {setInput("confirm");setClick(!click)}}>Accept</button>
+        <button onClick={() => {setInput("start");setClick(!click)}}>Reset</button>
       </div>
     </div>
   );
