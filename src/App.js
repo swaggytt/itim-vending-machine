670be2a6ba741b03b72edbf120299a930cba4db4
Power1stZ <61010568@kmitl.ac.th>
2021-04-06 12:58:58 +0700

    finish the result component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home}></Route>
+        <Route exact path='/' component={Home}></Route>
       </Switch>
     </Router>
   );
