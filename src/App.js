957bd370b4587ca5c6b6f49104f3dd02d2894a10
Puwadol Limwanichsin <45484127+AlbertPuwadol@users.noreply.github.com>
2021-04-08 22:57:29 +0700

    add input tape

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [input, setInput] = useState("start");
   const [click, setClick] = useState(true);
   const [finish, setFinish] = useState(false);
+  const [tape, setTape] = useState("");
   const { key, forceUpdate } = useForceUpdate();
 
   const [Modal, open, close, isOpen] = useModal("root", {
@@ -27,6 +28,13 @@ function App() {
       open();
       setFinish(true);
     }
+    
+    if(input === "reset"){
+      setTape("")
+    }else if(input !== "start"){
+      setTape(tape + " " + input);
+    }
+    
     // console.log("force")
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [click]);
@@ -36,7 +44,9 @@ function App() {
       {/* <LogoNavbar /> */}
       <div key={key} className="state-diagram">
         <Diagram value={input} click={click} />
+        <div className="input-tape">{tape}</div>
       </div>
+      
       <div>
         <ImageResult state={parseInt(localStorage.getItem("state"))} />
         <InputPanel setInput={setInput} setClick={setClick} click={click} />
@@ -46,7 +56,9 @@ function App() {
         <div className="Container">
           <div className="Result">Your Result</div>
           <ImageResult state={parseInt(localStorage.getItem("prevState"))} />
-          <button className="close-button" onClick={close}>CLOSED</button>
+          <button className="close-button" onClick={close}>
+            CLOSED
+          </button>
         </div>
       </Modal>
     </div>
