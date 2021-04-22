7dbbaaec24624b50c456d2fe8c30abf38e84fd5f
swaggytt <ichromex@hotmail.com>
2021-04-22 19:39:30 +0700

    Embeded Youtube link

--- src/components/Information/index.js
+++ src/components/Information/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useModal from "react-hooks-use-modal";
 import "./information.css";
+import ReactPlayer from "react-player";
 
 const Information = () => {
   // eslint-disable-next-line
@@ -9,21 +10,25 @@ const Information = () => {
   });
   return (
     <div>
-      <span class='dot' onClick={open}>
+      <span class="dot" onClick={open}>
         !
       </span>
       <Modal>
-        <div className='Container'>
-          <div className='Header'>
+        <div className="Container">
+          <div className="Header">
             <h1>How to use</h1>
           </div>
 
-          <div className='Information'>
-            <p>อยากจะใส่อะไรก็ใส่ไปเลย</p>
+          <div className="Information">
+            <p>
+              <ReactPlayer url="https://www.youtube.com/watch?v=1nmOuhh4VOg" />
+            </p>
           </div>
 
-          <div className='Button'>
-            <button onClick={close} className="close-button">CLOSE</button>
+          <div className="Button">
+            <button onClick={close} className="close-button">
+              CLOSE
+            </button>
           </div>
         </div>
       </Modal>
